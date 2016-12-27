a5362070039bdd310a3a359415128c3014382262
chen <chen@chendeMacBook-Pro.local>
2016-12-27 11:20:20 +0800

    native和h5窗口能跳转

--- MySwiftProject/H5ViewController.swift
+++ MySwiftProject/H5ViewController.swift
@@ -1,32 +1,26 @@
 //
-//  ViewController.swift
+//  H5ViewController.swift
 //  MySwiftProject
 //
 //  Created by chen on 2016/12/27.
 //  Copyright © 2016年 chen. All rights reserved.
 //
 
-
 import UIKit
 
-// class ViewController: CDVViewController {
-class ViewController: UIViewController {
-    @IBOutlet weak var myLabel: UILabel!
-
-    @IBAction func go(_ sender: UIButton) {
-        self.myLabel.text = "hello world!"
-    }
-
+class H5ViewController: CDVViewController {
+// class H5ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
