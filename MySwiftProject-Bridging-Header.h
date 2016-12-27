0c12aaf8d767df44868c1c7984837986ff1e40fb
chen <chen@chendeMacBook-Pro.local>
2016-12-27 10:10:23 +0800

    配置好了cordova

---
+++ MySwiftProject-Bridging-Header.h
@@ -0,0 +1,6 @@
+
+#import <Cordova/CDVViewController.h>
+#import <Cordova/CDVCommandDelegateImpl.h>
+#import <Cordova/CDVCommandQueue.h>
+#import <Cordova/CDVPlugin.h>
+
---
+++ MySwiftProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MySwiftProject
+//
+//  Created by chen on 2016/12/27.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: CDVViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
